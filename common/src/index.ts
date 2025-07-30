7f514c24bca2c17f3fb512f13ea1d6e961caba66
theaditya09 <adityarajpandit2004@gmail.com>
2025-07-30 14:15:37 +0530

    added zod validation

--- common/src/index.ts
+++ common/src/index.ts
@@ -1,13 +1,13 @@
 import z from "zod"
 
 export const signupInput = z.object({
-    email : z.email(),
+    email : z.string().email(),
     password : z.string().min(6),
     name : z.string().optional()
 })
 
 export const signinInput = z.object({
-    email : z.email(),
+    email : z.string().email(),
     password : z.string().min(6),
 })
 
